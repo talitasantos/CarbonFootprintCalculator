6ba3bddb870bbc116b28cfa59dbb1fd7773f287f
talitasantos2310@gmail.com <Maycon@1802>
2023-02-10 21:35:22 -0300

    Carbon Footprint Calculator Locators file creation

---
+++ cypress/support/locators/CarbonFootprintCalculatorLocators.js
@@ -0,0 +1,49 @@
+const locators = {
+
+    // HOME ENERGY TAB
+    PRIMARY_HEATING_SOURCE_SELECT: `[id="primaryHeatingSource"]`,
+    NATUAL_GAS_INPUT: `[id="naturalGasTextInput"]`,
+    NATUAL_GAS_SELECT: `[id="naturalGasSelectInput"]`,
+    ELETRICY_IPUNT: `[id="electricityTextInput"]`,
+    ELETRICY_SELECT: `[id="electricitySelectInput"]`,
+    FUEL_INPUT: `[id="fuelTextInput"]`,
+    FUEL_SELECT: `[id="fuelSelectInput"]`,
+    PROPANE_INPUT: `[id="propaneTextInput"]`,
+    PROPANE_SELECT: `[id="propaneSelectInput"]`,
+    ENERGY_AC_INPUT: `[id="energyAC"]`,
+    ENERGY_HEAT_INPUT: `[id="energyHeat"]`,
+    NUMBER_OF_LIGHTS_INPUT: `[id="lightsToReplace"]`,
+    POWER_MANAGMENT_SELECT: `[id="pwrMgmtSelect"]`,
+    INCREAD_POWER_USAGE: `[id="increaseGreenInput"]`,
+    REFRIGERATOR_SELECT: `[id="coldWaterSelect"]`,
+    FURNACE_SELECT: `[id="furnaceSelect"]`,
+    WINDOM_SELECT: `[id="windowSelect"]`,
+    GO_TO_TRANSPORTATION_BUTTON: `[id="to-transportation"]`,
+   
+    // TRANSPORTATION TAB
+    NUMBER_VEHICLES_INPUT: `[id="numVehiclesInput"]`,
+    CURRENT_MAIN_SELECT:`[id="maintCurrentSelect"]`,
+    MILES_INPUT: `[id="vehicle1Miles"]`,
+    MILES_SELECT: `[id="vehicle1Select"]`,
+    GALES_MILEAGE_INPUT: `[id="vehicle1GasMileage"]`,
+    GO_TO_WASTE_BUTTON: `[id="to-waste"]`,
+
+    // WASTE TAB
+    ALUMINUM_CHECKBOK: `input[type="checkbox"][id="aluminumCheckbox"]`,
+    PLASTIC_CHECKBOK: `input[type="checkbox"][id="plasticCheckbox"]`,
+    GLASS_CHECKBOK: `input[type="checkbox"][id="glassCheckbox"]`,
+    NEWSPAPER_CHECKBOK: `input[type="checkbox"][id="newspaperCheckbox"]`,
+    MAGAZINES_CHECKBOK: `input[type="checkbox"][id="magazinesCheckbox"]`,
+    ALUMINUM_REDUCE_CHECKBOK: `input[type="checkbox"][id="aluminumCheckboxR"]`,
+    PLASTIC_REDUCE_CHECKBOK: `input[type="checkbox"][id="plasticCheckboxR"]`,
+    GLASS_REDUCE_CHECKBOK: `input[type="checkbox"][id="glassCheckboxR"]`,
+    NEWSPAPER_REDUCE_CHECKBOK: `input[type="checkbox"][id="newspaperCheckboxR"]`,
+    MAGAZINES_REDUCE_CHECKBOK: `input[type="checkbox"][id="magazinesCheckboxR"]`,
+    CONTINUE_TO_REPORT_BUTTPN: `[id="to-report"]`,
+
+    PRINT_ICON_BUTTON: `[id="printicon"]`
+
+
+}
+
+export default locators;
